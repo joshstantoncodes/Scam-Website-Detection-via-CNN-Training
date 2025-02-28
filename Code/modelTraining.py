8a2443355a1465622ba8953d431e55bcf557c940
Josh Stanton <josh.stanton@live.ca>
2025-02-28 10:00:15 -0400

    Update modelTraining.py

--- Code/modelTraining.py
+++ Code/modelTraining.py
@@ -24,12 +24,12 @@ KL = keras.layers
 
 
 # Loading all PNG files in a single run is extremely memory intensive and time-consuming,
-scam_full_batch_path = r"C:\Users\joshs\Documents\GitHub\Scam-Website-Detection-via-CNN-Training\Scam Websites\Scam Site Captures"
-scam_small_batch_path = r"C:\Users\joshs\Documents\GitHub\Scam-Website-Detection-via-CNN-Training\Scam Websites\Scam Sites Small Batch"
+scam_full_batch_path = r"C:\Users\joshs\Documents\GitHub\Scam-Website-Detection-via-CNN-Training\Training Data\Scam Websites\Scam Site Captures"
+scam_small_batch_path = r"C:\Users\joshs\Documents\GitHub\Scam-Website-Detection-via-CNN-Training\Training Data\Scam Websites\Scam Sites Small Batch"
 
 # for the time being, while still building, using a smaller batch would be better.
-legitimate_full_batch_path = r"C:\Users\joshs\Documents\GitHub\Scam-Website-Detection-via-CNN-Training\Legitimate Websites\Legitimate Captures"
-legitimate_small_batch_path = r"C:\Users\joshs\Documents\GitHub\Scam-Website-Detection-via-CNN-Training\Legitimate Websites\Legitimate Site Small Batch"
+legitimate_full_batch_path = r"C:\Users\joshs\Documents\GitHub\Scam-Website-Detection-via-CNN-Training\Training Data\Legitimate Websites\Legitimate Captures"
+legitimate_small_batch_path = r"C:\Users\joshs\Documents\GitHub\Scam-Website-Detection-via-CNN-Training\Training Data\Legitimate Websites\Legitimate Site Small Batch"
 
 
 '''
